c799c0539fc6bcdbf1a7773ab9e2c6d06ef491fe
baoobab <v.xxl.90@mail.ru>
2024-08-21 00:44:32 +0300

    saving to file added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 import puppeteer from 'puppeteer';
+import fs from 'fs';
 
+const saveProductDataInFile = async (page) => {
+    const priceDiv = await page.$('.PriceInfo_root__GX9Xp');
+    const productData = await page.evaluate((priceDiv) => {
+        const rating = document.querySelector('.ActionsRow_stars__EKt42')?.textContent || "0";
+        const reviewCount = document.querySelector('.ActionsRow_reviews__AfSj_')?.textContent || "0";
+        if (priceDiv.querySelector(".Price_role_discount__l_tpE")) {
+            const price = priceDiv.querySelector('.Price_role_discount__l_tpE')?.textContent || "0";
+
+            const priceOld = priceDiv.querySelector('.Price_role_old__r1uT1')?.textContent || "0";
+
+            return [
+                `price=${parseFloat(price
+                    .replace(",", ".")
+                    .replace(/[^0-9.]+/g, ""))}`,
+                `priceOld=${parseFloat(priceOld
+                    .replace(",", ".")
+                    .replace(/[^0-9.]+/g, ""))}`,
+                `rating=${parseFloat(rating
+                    .replace(/[^0-9.]+/g, ""))}`,
+                `reviewCount=${Number(reviewCount
+                    .replace(/[^0-9]+/g, ""))}`
+            ].join('\n')
+        } else {
+            const price = priceDiv.querySelector('.Price_role_regular__X6X4D')?.textContent || "0";
+            return [
+                `price=${parseFloat(price
+                    .replace(",", ".")
+                    .replace(/[^0-9.]+/g, ""))}`,
+                `rating=${parseFloat(rating
+                    .replace(/[^0-9.]+/g, ""))}`,
+                `reviewCount=${Number(reviewCount
+                    .replace(/[^0-9]+/g, ""))}`
+            ].join('\n')
+        }
+    }, priceDiv);
+
+    fs.writeFileSync('product.txt', productData);
+}
 
 const scrollToBottom = async (page) => {
     await page.evaluate(async () => {
@@ -71,10 +110,14 @@ async function main() {
         });
         await page.reload();
 
+        // Ждем проверку браузера
+        await page.waitForSelector(".c1", {hidden: true, timeout: 10000})
         // Ждем загрузки контейнера с регионами, и открываем модалку
-        await page.waitForSelector(".Region_regionIcon__oZ0Rt", {timeout: 10000})
+        await page.waitForSelector(".Region_regionIcon__oZ0Rt", {visible: true, timeout: 10000})
+        await page.waitForResponse("https://www.vprok.ru/web/api/v1/regionList", {timeout: 10000});
+
         await page.locator(".Region_regionIcon__oZ0Rt").click();
-        await page.waitForSelector(".UiRegionListBase_list__cH0fK", {timeout: 10000});
+        await page.waitForSelector(".UiRegionListBase_list__cH0fK", {visible: true, timeout: 10000});
 
         // Проверка на наличие региона в списке
         if (!await checkRegion(page, regionName)) {
@@ -82,14 +125,12 @@ async function main() {
             console.error(`Error: Region isn't on the site`)
             return;
         }
+        // Выбор региона
         await page.locator(`text/${regionName}`).click();
 
-        // Ждем полной загрузки изображения товара
-        await page.waitForFunction(() => {
-            const img = document.querySelector('img.UiSharedPicture_image___6F3C');
-            return img && img.complete && img.naturalHeight !== 0;
-        });
-
+        // Сохранение данных в файл после закрытия модалки
+        await page.waitForSelector('.UiRegionListBase_list__cH0fK', {hidden: true, timeout: 10000});
+        await saveProductDataInFile(page);
 
         await scrollToBottom(page); // Прокрутка до низа страницы для корректной отрисовки
         await hideUselessElements(page); // Скрытие лишних элементов для скриншота
@@ -104,12 +145,10 @@ async function main() {
             path: "screenshot.jpg", fullPage: true
         });
 
-        console.log("Done");
+        console.log("Done, check screenshot.jpg and product.txt files in project folder");
     } catch (e) {
         console.error(e)
     }
 
     await browser.close();
 }
-
-await main();
